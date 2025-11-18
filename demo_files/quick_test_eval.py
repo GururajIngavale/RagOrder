7db9a465c0eacff8fdba2625c6970cedf11f17a7
Gururaj Ingavale <156651899+GururajIngavale@users.noreply.github.com>
2025-11-18 23:30:38 +0530

    Delete demo_files directory

--- demo_files/quick_test_eval.py
+++
@@ -1,9 +0,0 @@
-from evaluate_strategy import evaluate_strategy
-
-# sample indicators
-ind = {"sma20": 10, "sma50": 9, "rsi": 55}
-
-rule = "sma20 > sma50 and rsi < 70"
-
-print("Rule:", rule)
-print("Evaluation:", evaluate_strategy(rule, ind))
--- demo_files/quick_test_verify.py
+++
@@ -1,9 +0,0 @@
-from verify_db import query_strategy
-
-ind = {
-    "sma20": 1820.4,
-    "sma50": 1815.1,
-    "rsi": 63
-}
-
-print(query_strategy(ind))
